dcba6515767b3c1dd304c7f23c1f74b56205831b
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-02-10 09:10:25 -0500

    user can delete movies from the list

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ const App = (props) => {
   }, []);
 
   const deleteMovie = (id)=> {
+    setMovies(movies.filter(item => (item.id !== Number(id))));
   }
 
   const addToFavorites = (movie) => {
@@ -48,7 +49,7 @@ const App = (props) => {
             </Route>
 
             <Route path="/movies/:id">
-              <Movie/>
+              <Movie deleteMovie={deleteMovie} />
             </Route>
 
             <Route path="/movies">
